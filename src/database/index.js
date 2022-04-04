0d5c51a9ed4e5d6f14f9ad35fc45f39005677e02
Nelson Javier dos Santos Baez <bluexbaez@gmail.com>
2022-04-04 18:39:35 -0300

    corrigido conflito

--- src/database/index.js
+++ src/database/index.js
@@ -7,14 +7,11 @@ const Customer = require('../app/models/Customer');
 const AgeGroup = require('../app/models/AgeGroup');
 const Gender = require('../app/models/Gender');
 const Product = require('../app/models/Product');
-<<<<<<< HEAD
 const ProductPrice = require('../app/models/ProductPrice');
 const Commission = require('../app/models/Commission');
-=======
 const Size = require('../app/models/Size');
 const RefreshToken = require('../app/models/RefreshToken');
 const Permission = require('../app/models/Permission');
->>>>>>> a37a38f1813309d1da8a3842207c87992ce9edf2
 
 const connection = new Sequelize(dbConfig);
 
@@ -24,27 +21,21 @@ Customer.init(connection);
 AgeGroup.init(connection);
 Gender.init(connection);
 Product.init(connection);
-<<<<<<< HEAD
 Commission.init(connection);
 ProductPrice.init(connection);
 
-=======
 Size.init(connection);
 RefreshToken.init(connection);
 Permission.init(connection);
->>>>>>> a37a38f1813309d1da8a3842207c87992ce9edf2
 
 Role.associate(connection.models);
 User.associate(connection.models);
 AgeGroup.associate(connection.models);
 Gender.associate(connection.models);
 Product.associate(connection.models);
-<<<<<<< HEAD
 Commission.associate(connection.models);
 ProductPrice.associate(connection.models);
-=======
 RefreshToken.associate(connection.models);
 Permission.associate(connection.models);
->>>>>>> a37a38f1813309d1da8a3842207c87992ce9edf2
 
 module.exports = connection;
