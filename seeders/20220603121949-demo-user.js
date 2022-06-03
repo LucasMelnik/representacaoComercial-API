a72373e9b4422bb4f3eb51f3ecbbb5e7aae37c39
bluexbaez <bluexbaez@gmail.com>
2022-06-03 09:52:17 -0300

    Adicinado .env.example e seed de usuario teste

---
+++ seeders/20220603121949-demo-user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const bcrypt = require("bcryptjs/dist/bcrypt");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [{
+      firstname: 'zaca',
+      lastname: 'rias',
+      email: 'zacarias@teste.com',
+      password: bcrypt.hashSync('lucasbonito', 8),
+      created_at: new Date(),
+      updated_at: new Date()
+    }]);
+  },
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
